2efdcfa5e7c14e174e16c203aac407e000b15a55
lilith1228 <2005012nov.darshikaagrawal@gmail.com>
2025-05-29 12:11:18 +0000

    git commit -m "python_task"

---
+++ python_task/chatbot.py
@@ -0,0 +1,15 @@
+s1 = "hello"
+s2 = "how are you"
+s3 = "bye"
+
+input1 = input().lower()
+
+if input1== s1:
+    print("hii")
+elif input1 == s2:
+    print("I'm fine, thanks!")
+elif input1 == s3:
+    print("goodbye")
+else:
+    print("I didn't understand that.")
+
